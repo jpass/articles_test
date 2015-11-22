ce37925011d3d381840f1ec65ea5069acb95ec7d
jpass <suchomski1@gmail.com>
2015-11-23 00:24:56 +0100

    added jms serializer bundle and fos rest bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,8 @@ class AppKernel extends Kernel
 
             new FOS\UserBundle\FOSUserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
